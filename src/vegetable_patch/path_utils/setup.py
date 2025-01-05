8bc3f04c4a3616bfdb9a8dc382d46df541d75a49
TheBioinformaticalGardener <167814751+TheBioinformaticalGardener@users.noreply.github.com>
2025-01-05 09:41:48 +0100

    Removed earlier attempt to create a package

--- src/vegetable_patch/path_utils/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="project",
-    version="0.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    install_requires=[],  # List dependencies if needed
-)
