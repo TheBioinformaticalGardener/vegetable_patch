5b134ed98624974dff3d9c137ae2075254fb7d37
TheBioinformaticalGardener <167814751+TheBioinformaticalGardener@users.noreply.github.com>
2025-01-05 09:46:10 +0100

    Import all content of sub-repositories

--- src/vegetable_patch/__init__.py
+++ src/vegetable_patch/__init__.py
@@ -3,3 +3,7 @@
 __author__ = """Marius Dobbe Klemetsen"""
 __email__ = 'marius.klemetsen@outlook.com'
 __version__ = '0.1.0'
+
+from .dataframe_utils import *
+from .path_utils import *
+from .string_utils import *
